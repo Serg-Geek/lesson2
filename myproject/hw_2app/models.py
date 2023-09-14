ed9f559b943f7f632ac2c13c99bd839ceaff4aa1
Serg-Geek <110627203+Serg-Geek@users.noreply.github.com>
2023-09-14 19:02:51 +0400

    add hw 2

---
+++ myproject/hw_2app/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    registration_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+# Модель "Товар"
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    added_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+# Модель "Заказ"
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    order_date = models.DateField()
+
+    def __str__(self):
+        return f"Order by {self.client.name} - {self.order_date}"
